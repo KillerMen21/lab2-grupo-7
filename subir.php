07d52d38b492408b51942e731a01b883be31c17b
Joseph-gamboa <josephporras555@gmail.com>
2026-09-10 09:51:56 -0600

    Cambios realizados

--- subir.php
+++ subir.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+$archivo = $_FILES['archivo'];
+
+$nombre = $archivo['name'];
+$tmp = $archivo['tmp_name'];
+
+move_uploaded_file($tmp, "uploads/" . $nombre);
+
+echo "Archivo subido";
+?>
