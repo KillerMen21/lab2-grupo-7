7611b9b30bff6022a22c8355c3cedb6404c06166
JohanCrdas <johancrdas@gmail.com>
2026-09-10 10:18:23 -0600

    Eliminacion de cosas

--- index.php
+++ index.php
@@ -4,15 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>lab2-grupo 7</title>
-=======
     <title>Lab 2 - Grupo 7</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
           rel="stylesheet">
 
->>>>>>> 6f243463a59d989389c9f46f422662595c17077d
     <link rel="stylesheet" href="style.css">
 </head>
 
